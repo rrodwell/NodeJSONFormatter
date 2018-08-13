d78fbffd25a3ff7c20c8f6fad897557ad3acce80
Ryan Rodwell <ryan.rodwell@accesscfa.com>
2018-08-13 17:08:36 -0400

    call to order API

--- sort.js
+++ sort.js
@@ -1,55 +1,88 @@
-var fs = require("fs");
-
+var fs = require('fs');
+var fetch = require('fetch').fetchUrl;
 
 //Files
 var firstFile = process.argv[2];
 // var secondFile =  process.argv[3];
 
-//fs read file function
-function read(fileName) {
 
-    var cleanData;
-    fs.readFile(fileName, "utf8", (err,stream) => {
-        if (err){ 
-            console.log(err); 
+function callToOrderAPI() {
+
+    fetch('https://order.api.my.chick-fil-a.com/orders/menu/nutrition/1.0', (err, response, body) => {
+        if(err){
+            console.log(err)
         }
+        var data = JSON.parse(body);
 
-        var data = JSON.parse(stream)
         console.log(data.items.length)
         var tags = []
         var itemsObj = {
-            items:[]
+            items: []
         }
         //Capture nutrition tags
-        for(var i=0;i<data.items.length; i++){
+        for (var i = 0; i < data.items.length; i++) {
             tags.push(data.items[i].tag)
         }
         sortedTags = tags.sort()
         console.log(sortedTags)
 
-        for(var j=0;j<sortedTags.length;j++){
+        for (var j = 0; j < sortedTags.length; j++) {
             for (var k = 0; k < data.items.length; k++) {
-                if(sortedTags[j] == data.items[k].tag){
+                if (sortedTags[j] == data.items[k].tag) {
                     itemsObj.items.push(data.items[k])
                 }
             }
         }
 
-        // console.log(itemsObj)
-        //Write new files will sorted data
         writeFile(itemsObj);
     })
 }
 
+// //fs read file function
+// function sort(fileName) {
+
+//     var cleanData;
+//     fs.readFile(fileName, 'utf8', (err,stream) => {
+//         if (err){ 
+//             console.log(err); 
+//         }
+
+//         var data = JSON.parse(stream)
+//         console.log(data.items.length)
+//         var tags = []
+//         var itemsObj = {
+//             items:[]
+//         }
+//         //Capture nutrition tags
+//         for(var i=0;i<data.items.length; i++){
+//             tags.push(data.items[i].tag)
+//         }
+//         sortedTags = tags.sort()
+//         console.log(sortedTags)
+
+//         for(var j=0;j<sortedTags.length;j++){
+//             for (var k = 0; k < data.items.length; k++) {
+//                 if(sortedTags[j] == data.items[k].tag){
+//                     itemsObj.items.push(data.items[k])
+//                 }
+//             }
+//         }
+
+//         // console.log(itemsObj)
+//         //Write new files will sorted data
+//         writeFile(itemsObj);
+//     })
+// }
+
 function writeFile(data){
-    fs.writeFile("nutritionSorted2.json", JSON.stringify(data, null, 4), (err) => {
+    fs.writeFile('nutrition.json', JSON.stringify(data, null, 4), (err) => {
         if (err) {
             console.error(err);
             return;
         };
-        console.log("File has been created");
+        console.log('File has been created');
     });
 }
 
 //Run Function
-read(firstFile);
+callToOrderAPI(firstFile);
